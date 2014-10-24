126c936b4e023da0d8223bc9ecd8865708d3ae8d
zh2413709 <zhe2@student.rcc.edu>
2014-10-24 18:53:34 +0000

    This is the first program of the midterm

--- Hmwk/midterm/gross_pay.s
+++ Hmwk/midterm/gross_pay.s
@@ -6,8 +6,6 @@ message2 : .asciz "How much do you get paid for each hour:"
 .balign 4
 message3 : .asciz "You worked %d hr,you are paid $ %d per-hr, and your grosspay is $ %d\n"
 .balign 4
-message4 : .asciz "You are working too much!"
-.balign 4
 scan_pattern : .asciz "%d"
 .balign 4
 number_read1 : .word 0
@@ -20,28 +18,32 @@ calculation:
 
 	cmp r1, #60
 	ble lesser_equal_sixty_hours
+	mov r3, #0
 end:
 	pop {lr}
 	bx lr
 lesser_equal_sixty_hours:
 	cmp r1, #40
 	ble lesser_equal_forty_hours
-	mul r3, r1, #3
+	mov r0, #3
+	mul r3, r1, r0
 	sub r3, r3, #60
 	mul r3, r2, r3
 	b end
 lesser_equal_forty_hours:
 	cmp r1, #20
 	ble lesser_equal_twenty_hours
-	mul r3, r1, #2
+	mov r0, #2
+	mul r3, r1, r0
 	sub r3, r3, #20
 	mul r3, r2, r3
 	b end
-lesser_euqal_twenty_hours:
+lesser_equal_twenty_hours:
 	mul r3, r1, r2
-	b end 
-.global gross_pay
-gross_pay:
+	b end
+ 
+.global main
+main:
 	push {lr}
 
 	ldr r0, address_of_message1
@@ -64,25 +66,15 @@ gross_pay:
         ldr r2, [r0]
         bl calculation
 
-	ldr r1, address_of_number_read1
-	ldr r1, [r1]
-	cmp r1, #60
-	bgr work_too_much
-
 	ldr r0, address_of_message3
 	bl printf
 
 	pop {lr}
 	bx lr
-work_too_much:
-	ldr r0, address_of_message4
-	printf
-	b gross_pay
 
 address_of_message1 : .word message1
 address_of_message2 : .word message2
 address_of_message3 : .word message3
-address_of_message4 : .word message4
 address_of_scan_pattern : .word scan_pattern
 address_of_number_read1 : .word number_read1
 address_of_number_read2 : .word number_read2
