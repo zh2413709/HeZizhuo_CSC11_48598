2b8a8b9e8e00d5d3a6d55d7d21fd0b7e3ac9a6be
Zizhuo He <zh2413709@CSC11.(none)>
2014-10-25 09:07:31 -0700

    modified package.s

--- Hmwk/midterm/package.s
+++ Hmwk/midterm/package.s
@@ -1,12 +1,42 @@
 .data
 .balign 4
-message1 :.ascizr "Hey, choose one of the packag ('a', 'b', 'c'):"
+message1 :.asciz "Hey, choose one of the packag ('a', 'b', 'c'): \n "
 .balign 4
 scan_pattern : .asciz "%c"
 .balign 4
-message2 : .asciz "You just entered an invaluable value: %c, please enter again. \n"
+message2 : .asciz "You entered an invaluable value: %c \n "
+.balign 4
+message3 : .asciz "You have entered: %c, thank you. \n"
 .balign 4
 read : .word 0
+
+.text
+caseA:
+	push {lr}
+
+	ldr r0, address_of_message3
+	bl printf
+
+	pop {lr}
+	bx lr
+
+caseB:
+	push {lr}
+
+        ldr r0, address_of_message3
+        bl printf
+
+        pop {lr}
+        bx lr
+
+caseC:
+	push {lr}
+
+        ldr r0, address_of_message3
+        bl printf
+
+        pop {lr}
+        bx lr
 
 .global main
 main:
@@ -33,21 +63,30 @@ options:
 	beq case_b
 	cmp r1, #'c'
 	beq case_c
-	cmp r1, 'C'
+	cmp r2, #'C'
 	beq case_c
 
 	ldr r0, address_of_message2
+	ldr r1, address_of_read
+	ldr r1, [r1]
         bl printf
-	b options
+	b end
 case_a:
-	ldr r0, address_of_message2
-	bl printf
+	bl caseA
+	b end
+case_b:
+	bl caseB
+	b end
+case_c:
+	bl caseC
+	b end
 end:
 	pop {lr}
 	bx lr
 
 address_of_message1 : .word message1
 address_of_message2 : .word message2
+address_of_message3 : .word message3
 address_of_scan_pattern : .word scan_pattern
 address_of_read : .word read
 .global printf
