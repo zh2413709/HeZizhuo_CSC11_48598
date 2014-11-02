77a5e529e9f4e854afffdd5d6bcb1449ce5cf4bc
Zizhuo He <zh2413709@CSC11.(none)>
2014-11-01 20:18:30 -0700

    unfinished project 1

--- Proj/proj1/rantest2.s
+++ Proj/proj1/rantest2.s
@@ -14,8 +14,8 @@ rantest2:
         bl srand                     /* Call srand */
         bl rand                      /* Call rand */
         mov r1,r0,ASR #2             /* In case random return is negative */
-	mov r3, r1
-	
+	mov r3, #3
+	mul r1, r1, r3
         mov r2,#7                   /* Move 90 to r2 */
                                          /* We want rand()%90+10 so cal divMod with rand()%90 */
         bl divMod                    /* Call divMod function to get remainder */
--- Proj/proj1/rantest3.s
+++ Proj/proj1/rantest3.s
@@ -14,7 +14,8 @@ rantest3:
         bl srand                     /* Call srand */
         bl rand                      /* Call rand */
         mov r1,r0,ASR #3             /* In case random return is negative */
-	add r1, r1, r4
+	mov r3, #4
+	mul r1, r1, r3
         mov r2,#7                   /* Move 90 to r2 */
                                          /* We want rand()%90+10 so cal divMod with rand()%90 */
         bl divMod                    /* Call divMod function to get remainder */
