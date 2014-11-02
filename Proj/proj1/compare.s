80e4801074b9ed81ba684015f1edbd3cbad9d606
zh2413709 <zhe2@student.rcc.edu>
2014-11-02 06:24:08 +0000

    pushing the AI of the game

---
+++ Proj/proj1/compare.s
@@ -0,0 +1,67 @@
+/* all of the comparision functions are here */
+.text
+
+.global cmp1
+cmp1:
+	push {lr}
+
+	cmp r4, r1
+	addeq r0, r0, #1
+	beq end
+	cmp r4, r2
+	addeq r5, r5, #1
+	beq end
+	cmp r4, r3
+	addeq r5, r5, #1
+end:
+	pop {lr}
+	bx lr
+
+.global cmp2
+cmp2:
+	push {lr}
+
+	cmp r4, r1
+	beq U2_equal_A1
+	cmp r4, r2
+	addeq r0, r0, #1
+	beq end
+	cmp r4, r3
+	addeq r5, r5, #1
+end:
+	pop {lr}
+	bx lr
+U2_equal_A1:
+	add r5, r5, #1
+	cmp r1, r2
+	addeq r0, r0, #1
+	subeq r5, r5, #1
+	b end
+
+.global cmp3
+cmp3:
+	push {lr}
+
+	cmp r4, r1
+	beq U3_equal_A1
+	cmp r4, r2
+	beq U3_equal_A2
+	cmp r4, r3
+	addeq r0, r0, #1
+end:
+	pop {lr}
+	bx lr
+U3_equal_A2:
+	add r5, r5, #1
+	cmp r2, r3
+	addeq r0, r0, #1
+	subeq r5, r5, #1
+	b end
+U3_equal_A1:
+	add r5, r5, #1
+	cmp r1, r3
+	addeq r0, r0, #1
+	subeq r5, r5, #1
+	b end
+
+
