f52fda01b35992d2994e5db3fcfefd14a4ce97d1
Zizhuo He <zh2413709@CSC11.(none)>
2014-11-05 10:59:54 -0800

    Pushing assignment6

---
+++ Hmwk/Collatz_Time_compare/time.s
@@ -0,0 +1,36 @@
+.data
+msg : .asciz "Starting: %d\nEnding: %d\nDelta: %d\n"
+start : .word 0
+
+.text
+
+.global starting
+starting:
+	push {lr}
+
+	mov r0, #0
+	bl time
+	ldr r2, addr_of_start
+	str r0, [r2]
+
+	pop {lr}
+	bx lr
+
+.global ending
+ending:
+	push {lr}
+
+	mov r0, #0
+	bl time
+	mov r2, r0
+	ldr r1, addr_of_start
+	ldr r1, [r1]
+	sub r3, r2, r1
+	ldr r0, addr_of_msg
+	bl printf
+
+	pop {lr}
+	bx lr
+addr_of_start : .word start
+addr_of_msg : .word msg
+.global printf
