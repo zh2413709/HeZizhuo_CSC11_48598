ffd145168e5c14594e0cf664fae8777736091e1e
Zizhuo He <zh2413709@CSC11.(none)>
2014-11-14 21:42:00 -0800

    Updating the mian of assignment7a

--- Hmwk/Temperature/assignment7a.s
+++ Hmwk/Temperature/assignment7a.s
@@ -12,10 +12,10 @@ msg4 : .asciz "====================Invalid number, please try again!============
 temp_convert:
 	push {lr}
 
-	sub r1, r1, #32
+	sub r1, r1, #32 '/* r1 - 32 */
 	add r1, r1, r1, LSL #2 /* r1 ← r1 + (r1*4) equivalent to r1 ← r1*5 */
-	mov r2, #9
-	bl divMod
+	mov r2, #9 /* #9 is the denominator */
+	bl divMod /* call divMod function to do the division */
 
 	pop {lr}
 	bx lr
@@ -24,39 +24,39 @@ temp_convert:
 main:
 	push {lr}
 
-	ldr r0, addr_of_msg1
-	bl printf
+	ldr r0, addr_of_msg1 /* load the address of the first message */
+	bl printf /* call printf*/
 ask_for_input:
-	ldr r0, addr_of_msg2
-	bl printf
+	ldr r0, addr_of_msg2 /* load the address of the second message */
+	bl printf /* call printf */
 
-	ldr r0, addr_of_scan_format
-	ldr r1, addr_of_temp_read
-	bl scanf
+	ldr r0, addr_of_scan_format /* load the address of the scan format */
+	ldr r1, addr_of_temp_read /* load the address of the temperature that user entered */
+	bl scanf /& call scanf */
 
-	ldr r0, addr_of_temp_read
-	ldr r1, [r0]
+	ldr r0, addr_of_temp_read /* load the address of the user entered temperature */
+	ldr r1, [r0] /* load the content of the address */
 
-	cmp r1, #32
-	bmi invalid
+	cmp r1, #32 /* compare entered temperature to 32 */
+	bmi invalid /* If it is smaller than 32, it boecomes invalid number */
 
-	cmp r1, #212
-	bgt invalid
+	cmp r1, #212 /* compare entered temperature to 212 */
+	bgt invalid /* If it is larger than 212, it becomnes invalid as well */
 
-	bl temp_convert
+	bl temp_convert /* call the temperature converter function */
 
-	mov r2, r0
-	ldr r1, addr_of_temp_read
-	ldr r1, [r1]
-	ldr r0, addr_of_msg3
-	bl printf
+	mov r2, r0  /* save the result in Celsius to r2 */
+	ldr r1, addr_of_temp_read /* load the address of entered number */
+	ldr r1, [r1] /* load the content of address */
+	ldr r0, addr_of_msg3 /* load the address of message3 */
+	bl printf /* call printf */
 
 	pop {lr}
 	bx lr
 invalid:
-	ldr r0, addr_of_msg4
-	bl printf
-	b ask_for_input
+	ldr r0, addr_of_msg4 /* load the address of message4 */
+	bl printf /* call printf */
+	b ask_for_input /* branch to the beginning when the user enter an invalid number */ 
 
 addr_of_msg1 : .word msg1
 addr_of_msg2 : .word msg2
