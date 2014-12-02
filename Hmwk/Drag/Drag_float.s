3c043d104bf5a811a94e3ec3bbf180e16054ecd8
Zizhuo He <zh2413709@CSC11.(none)>
2014-12-01 20:28:03 -0800

    pushing drag_int and drag_float

---
+++ Hmwk/Drag/Drag_float.s
@@ -0,0 +1,96 @@
+.data
+Half: .float 0.5
+Pho: .float 0.002376
+Vel: .float 200.0
+Pi: .float 3.141592
+Rad: .float 6.0
+Conv: .float 0.00694
+Cd: .float 0.4
+thirty_two: .float 32.0
+
+msg: .asciz "Floating Dynamic Pressure = %f lbs\nCross Sectional Area x32 = %f ft^2\nInteger Drag x 32 = %f lbs\n"
+
+return: .word 0
+
+.text
+.global main
+.func main
+main:
+	ldr r1, addr_return
+	str lr, [r1]
+
+	sub sp, sp, #24
+
+	/* Dynp calculation */
+	ldr r0, addr_Half
+	vldr s14, [r0]
+	vcvt.f64.f32 d0, s14
+
+	ldr r0, addr_Pho
+	vldr s15, [r0]
+	vcvt.f64.f32 d1, s15
+
+	ldr r0, addr_Vel
+	vldr s16, [r0]
+	vcvt.f64.f32 d2, s16
+
+	vmul.f64 d4, d0, d1
+	vmul.f64 d4, d2, d4
+	vmul.f64 d4, d2, d4 @d4 has the value of Dynp
+
+	/* Area calculation */
+	ldr r0, addr_Pi
+	vldr s14, [r0]
+
+	ldr r0, addr_Rad
+	vldr s15, [r0]
+
+	ldr r0, addr_Conv
+	vldr s16, [r0]
+
+	vmul.f32 s14, s15, s14
+	vmul.f32 s14, s15, s14
+	vmul.f32 s14, s16, s14
+
+	vcvt.f64.f32 d5, s14 @d5 has the value of Area
+
+	/* Drag calculation */
+	ldr r0, addr_Cd
+	vldr s14, [r0] @s14 has the value of Cd as single
+	vcvt.f64.f32 d7, s14 @d7 has the value of Cd as double
+
+	vmul.f64 d6, d4, d5 @d6=d4*d5
+	vmul.f64 d6, d7, d6 @d6*=d7 now d6 has the value of Drag
+
+	/* prepare Area for display */
+	ldr r0, addr_thirty_two
+	vldr s14, [r0]
+	vcvt.f64.f32 d8, s14 @d8 has value of 32.00
+
+	vmul.f64 d5, d8, d5 @Area x 32
+
+	vmul.f64 d6, d8, d6 @Drag x 32
+
+	/* now, d4= Dynp, d5 = Area x 32, d6 = Drag x 32 */
+	ldr r0, =msg
+	vmov r2, r3, d4
+	vstr d5, [sp]
+	vstr d6, [sp, #+8]
+	bl printf
+	add sp, sp, #24
+
+	ldr r0, addr_return
+	ldr lr, [r0]
+	bx lr
+
+addr_Half: .word Half
+addr_Pho: .word Pho
+addr_Vel: .word Vel
+addr_Pi: .word Pi
+addr_Rad: .word Rad
+addr_Conv: .word Conv
+addr_Cd: .word Cd
+addr_thirty_two: .word thirty_two
+
+addr_return: .word return
+.global printf
---
+++ Hmwk/Drag/Drag_int.s
@@ -0,0 +1,87 @@
+.data
+iHalf : .word 1 @ 1 bit, >>1
+iRho : .word 0x9b5 @12 bits, >>20
+iVel : .word 200 @ 8bits
+iPi : .word 0x3243f7 @ 24 bits, >>20
+iRad : .word 6 @ 4 bits
+iConv : .word 0x1c7 @12 bits, >>16
+iCd : .word 0x666 @12 bits, >>12
+
+iDynp : .word 0 @Dynamic Pressure
+iArea : .word 0 @ Area
+iDrag : .word 0 @ Drag
+
+msg : .asciz "Integer Dynamic Pressure = %d lbs\nCross Sectional Area x32 = %d ft^2\nInteger Drag x 32 = %d lbs\n"
+.text
+.global main
+main:
+	push {lr}
+
+
+
+	ldr r0, addr_of_iHalf
+	ldr r1, [r0]
+	ldr r0, addr_of_iRho
+	ldr r2, [r0]
+	ldr r0, addr_of_iVel
+	ldr r3, [r0]
+	mul r4, r1, r2 @iDynp=iRho*iHalf; xBit 12, BP-21
+	mul r4, r3, r4 @iDynp*=iVel; xBit 20, BP-21
+	mul r4, r3, r4 @iDynp*=iVel; xBit 20, BP-21
+	mov r4, r4, lsr #12 @iDynp>>=12;  xBit 16, BP- 9
+	ldr r0, addr_of_iDynp
+	str r4, [r0]
+
+	ldr r0, addr_of_iPi
+	ldr r1, [r0] @ Pi
+	ldr r0, addr_of_iRad
+	ldr r2, [r0] @ Radius
+	ldr r0, addr_of_iConv
+	ldr r3, [r0]
+	mul r4, r1, r2 @iArea*=iRad; xBit 28, BP-20
+	mul r4, r2, r4 @iArea*=iRad; xBit 32, BP-20
+	mov r4, r4, lsr #12 @iArea>>=12;  xBit 20, BP- 8
+	mul r4, r3, r4 @iArea*=iConv; xBit 32, BP-24
+	mov r4, r4, lsr #16 @iArea>>=16;  xBit 16  BP- 8
+	ldr r0, addr_of_iArea
+	str r4, [r0]
+
+	ldr r0, addr_of_iDynp
+	ldr r1, [r0] @Dynp
+	ldr r0, addr_of_iArea
+	ldr r2, [r0] @Area
+	ldr r0, addr_of_iCd
+	ldr r3, [r0] @Cd
+	mul r4, r1, r2 @iDrag=iDynp*iArea; xBit 32 BP-17
+	mov r4, r4, lsr #12 @iDrag>>=12; xBit 20  BP- 5
+	mul r4, r3, r4 @iDrag*=iCd;  xBit 32  BP-17
+	ldr r0, addr_of_iDrag
+	str r4, [r0]
+
+	ldr r0, addr_of_iDynp
+	ldr r1, [r0]
+	ldr r0, addr_of_iArea
+	ldr r2, [r0]
+	ldr r0, addr_of_iDrag
+	ldr r3, [r0]
+	ldr r0, addr_of_msg
+	mov r1, r1, lsr #9
+	mov r2, r2, lsr #3
+	mov r3, r3, lsr #12
+	bl printf
+
+	pop {lr}
+	bx lr
+
+addr_of_iHalf : .word iHalf
+addr_of_iRho : .word iRho
+addr_of_iVel : .word iVel
+addr_of_iPi : .word iPi
+addr_of_iRad : .word iRad
+addr_of_iConv : .word iConv
+addr_of_iCd : .word iCd
+addr_of_iDynp : .word iDynp
+addr_of_iArea : .word iArea
+addr_of_iDrag : .word iDrag
+addr_of_msg : .word msg
+.global printf
