ec38bb6882cde76de916fb712ea0996891279671
Zizhuo He <zh2413709@CSC11.(none)>
2014-12-07 00:38:51 -0800

    add comments to the files

--- Proj/proj2/init_array.s
+++ Proj/proj2/init_array.s
@@ -2,11 +2,13 @@
 .text
 .global init_array
 init_array:
+	/* r0: # of items
+	   r1: address of array */
 	push {r4, lr}
 
-	mov r4, #0
-	mov r6, r1
-	mov r5, r0
+	mov r4, #0 /* the loop counter */
+	mov r6, r1 /* save a copy of address of array */
+	mov r5, r0 /* save a copy of # of items */
 
 	mov r0,#0                    /* Set time(0) */
     	bl time                      /* Call time */
@@ -14,35 +16,49 @@ init_array:
 
 	b check_loop_array
 loop_array:
-	bl rand                      /* Call rand */
-	mov r2, r0, lsr #7             /* Left shit r0 7 times to make sure the float does not overflow */
-	
-	vmov s1, r2
-	vcvt.f32.s32 s1, s1
+	bl rand       	 /* Call rand */
+	mov r2, r0, lsr #7	 /* Left shit r0 7 times to make sure the float does not overflow */
 
-	mov r2, #7
-	vmov s2, r2
-	vcvt.f32.s32 s2, s2
+/* The calculation part start from here */
+/* concept:
+	Since the idea behind utilizing DivMod is to get the remainder as the needed 1 bit decimal  random number, the same calculation can be represent by float.
+	Let #1 represents the random number generated from rand function.
+	Let #2.#3 represents the result of dividing #1 by 7.
+	#2 will be the integer part of the reuslt, while #3 is the fractional part.
+	The remainder is from subtracting the original number #1 by the result of multing the integer part #2 with 7.
+	Let #4 be the remainder, #4 equals #1-#2*7; #4=#1-#2*7.
+	In the following calculation:
+	1. Convert the random number from rand function to a single-precision float (s_float).
+	2. Use the div command to divide by float 7.
+	3. Convert the s_float to s_integer, which should only keep the integer part of it.
+	4. Then convert the ineger back to s_float again, multiply by 7.
+	5. Finally, subtract the original number by it to get the remainder. */
 
-	vdiv.f32 s3, s1, s2
+	vmov s1, r2	/* Bit copy from integer register r2 to s1 */
+	vcvt.f32.s32 s1, s1 /* Converts s1 signed integer value to a single-precision value and stores it in s1 */
 
-	vcvt.s32.f32 s3, s3
-	vmov r3, s3
+	mov r2, #7	 /* The goal is to generate 7 intege so the denominator is 7 */
+	vmov s2, r2 	/* Bit copy from integer register r2 to s2 */
+	vcvt.f32.s32 s2, s2 	/* Converts s2 signed integer value to a single-precision value and stores it in s2 */
 
-	vmov s4, r3
-	vcvt.f32.s32 s4, s4
-	vmul.f32 s5, s4, s2
-	vsub.f32 s6, s1, s5
+	vdiv.f32 s3, s1, s2 /* s3 is the result of dividing by 7; s3=s1/s2 */
 
-	vcvt.s32.f32 s6, s6
-	vmov r1, s6
+	vcvt.s32.f32 s3, s3 /* Converts s3 single-precision value to a signed integer and stores it in s3, in other words only keep the integer part of the float */
+	vmov r3, s3 /*Bit copy from s3 to r3 */
 
-	str r1, [r6, r4, lsl #2]
+	vmov s4, r3 /* Bit copy from integer register r3 to s4 */
+	vcvt.f32.s32 s4, s4 /* Converts s4 signed integer value to a single-precision value and stores it in s4 */
+	vmul.f32 s5, s4, s2 /* multiply the integer part of the float of division with float 7' s5=s4*s2 */
+	vsub.f32 s6, s1, s5 /* subtract the integer part of the float of division will leave us the remainder; s6=s1*s5 */
 
-	add r4, r4, #1
+	vcvt.s32.f32 s6, s6 /* Convert s6 single-precision value to a signed integer and stores it in s6 */
+	vmov r1, s6 /* Bit copy from s6 to r1 */
+	str r1, [r6, r4, lsl #2] /* store the integer into array */
+
+	add r4, r4, #1 /* increase the loop counter by 1 */
 check_loop_array:
-	cmp r4, r5
-	bne loop_array
+	cmp r4, r5 /* compare with # of items */
+	bne loop_array /* if not equal branch back */
 
 	pop {r4, lr}
 	bx lr
--- Proj/proj2/project2_main.s
+++ Proj/proj2/project2_main.s
@@ -1,4 +1,6 @@
 .data
+/* saving 3 integers into an array, each integer takes 4 bytes,
+ therefore align 3*4=12 bytes. Adding extra 4 bytes so 12+4=16 bytes */
 .align 4
 array: .skip 16
 .align 4
@@ -7,12 +9,14 @@ message1: .asciz "========This is a Mastermind game========\n========Please ente
 message2: .asciz "The result is RP: %d, WP: %d\n"
 .align 4
 scan_format: .asciz " %d %d %d"
+/* prepare variables for user input */
 .align 4
 spot1_read: .word 0
 .align 4
 spot2_read: .word 0
 .align 4
 spot3_read: .word 0
+
 .align 4
 message3: .asciz "========You have guessed all the number correctly, congratulations!========\n"
 .align 4
@@ -21,50 +25,52 @@ message4: .asciz "========This is the %d turns, you still have %d turns left====
 message5: .asciz "========You lose!========"
 
 .text
-.balign 4
+.align 4
 .global main
 main:
  	push {lr}
 
-generate_random_number:
-	mov r0, #3
-	ldr r1, =array
-	bl init_array
-
-	mov r9, #1 /* the turns counter */
-	mov r10, #8 /* turns left counter */
-guess:
 	ldr r0, =message1
 	bl printf
+/* generate three random number through utilizing floating numbers */
+generate_random_number:
+	mov r0, #3 /* load the # of items */
+	ldr r1, =array /* load the address of array */
+	bl init_array /* call function to fill array */
 
+/* set up loop counter here to limit the # of times user can try */
+	mov r9, #1 /* the turns counter */
+	mov r10, #8 /* turns left counter */
+
+guess:
 	ldr r0, =scan_format /* load the scan format */
 	ldr r1, =spot1_read /* load r1 address of variable4 */
 	ldr r2, =spot2_read /* load r2 address of variable5 */
 	ldr r3, =spot3_read /* load r3 address of variable6 */
 	bl scanf /*call scanf */
-	
+
 	/* compare*/
 	mov r0, #0 /* init 'right position counter' zero */
 	mov r5, #0 /* init 'wrong position counter' zero */
 
-	ldr r6, =array
+	ldr r6, =array /* load the address of array */
 	ldr r1, [r6] /* load r1 the first generated random number */
 
-	mov r7, #1 
+	mov r7, #1 /* increase the increment by 1 */
 	ldr r2, [r6, r7, lsl #2] /* load r2 the second generated random number */
 
-	mov r7, #2
+	mov r7, #2 /* increase the increment by 2 */
 	ldr r3, [r6, r7, lsl #2] /* load r3 the third generated random number */
 
-	ldr r4, =spot1_read
+	ldr r4, =spot1_read /* load the address of the first entered # */
 	ldr r4, [r4] /* load r4 the first number user entered */
 	bl cmp1 /* call AI to compare */
 
-	ldr r4, =spot2_read
+	ldr r4, =spot2_read /* load the address of the second entered # */
 	ldr r4, [r4] /* load r4 the second  number user entered */
 	bl cmp2 /* call AI to compare */
 
-	ldr r4, =spot3_read
+	ldr r4, =spot3_read /* load the address of the third entered # */
 	ldr r4, [r4] /* load r4 the third  number user entered */
 	bl cmp3 /* call AI to compare */
 
@@ -75,24 +81,24 @@ guess:
 	bl printf
 
 	ldr r0, =message4
-	mov r1, r9
-        sub r10, r10, #1 /*sub r7, to show how many turns left */
+	mov r1, r9 /* move r9 to r1, to show how many turns the user has tried */
+        sub r10, r10, #1 /*sub r10, to show how many turns left */
 	mov r2, r10
 	bl printf
 
 	add r9, r9, #1 /*add 1 to turns counter after each try */
 	cmp r9, #9
-	beq lose
-	cmp r4, #3 /* set up loop */
+	beq lose /* The user loses when the loop reaches the 9th turns */
+	cmp r4, #3 /* set up loop for second, third,... tries */
 	beq win /* if 'RP' reaches 3 means the user wins */
 	b guess /* loop back to input when user did not make the correct guess */
 lose:
 	ldr r0, =message5
-	bl printf
+	bl printf /* display losing message */
 	b end
 win:
 	ldr r0, =message3
-	bl printf
+	bl printf /* display winning message */
 end:
 	pop {lr}
 	bx lr
